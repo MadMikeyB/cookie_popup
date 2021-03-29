025213565e6880cb9bd5e4fe81bbdff5d049d335
Fahad Ali <fahad@larasoft.io>
2021-03-29 17:59:54 +0500

    code done

---
+++ src/Composers/ViewComposer.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace LarasoftIo\CookiePopup\Composers;
+
+
+
+use Illuminate\View\View;
+
+class ViewComposer
+{
+
+    public function compose(View $view)
+    {
+        $colors = config('cookie-popup.colour_scheme');
+        $text = config('cookie-popup.text');
+        $url_text = config('cookie-popup.url_text');
+        $cookie_policy_url = config('cookie-popup.cookie_policy_url');
+        $screen_position = config('cookie-popup.screen_position');
+        $border_radius = config('cookie-popup.border_radius');
+        $width = config('cookie-popup.width');
+        $height = config('cookie-popup.height');
+
+        $view->with('colors', $colors)
+        ->with('text', $text)
+        ->with('url_text', $url_text)
+        ->with('cookie_policy_url', $cookie_policy_url)
+        ->with('screen_position', $screen_position)
+        ->with('border_radius', $border_radius)
+        ->with('width', $width)
+        ->with('height', $height);
+    }
+}
